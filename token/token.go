83396c115ee6e5130fb38e96417a4a6b288a09d3
nmreadelf <frank.q.kong@hotmail.com>
2021-11-06 20:45:35 +0800

    add sometoken add support token: EOF, =, +, ,, ;, (, ), {, }, FUNCTION, LET

---
+++ token/token.go
@@ -0,0 +1,34 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifiers + literals
+	IDENT = "IDENT" // add, fooabr, x, y ...
+	INT   = "INT"   // 123456
+
+	// Operators
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywors
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
